32f716d519335a0b1384293e9da98ce8529a6935
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2020-12-02 23:25:25 -0500

    Day2 solved

--- day1.py
+++ day1.py
@@ -3,14 +3,6 @@ from math import prod
 
 
 def solve_day1(data, comb_len, target_sum):
-    return next(
-        prod(comb)
-        for comb in combinations(data, comb_len)
-        if sum(comb) == target_sum
-    )
-
-
-def main():
     """
     Solves day1 puzzle
     >>> in_data = [1721, 979, 366, 299, 675, 1456]
@@ -19,6 +11,14 @@ def main():
     >>> solve_day1(in_data, 3, 2020)
     241861950
     """
+    return next(
+        prod(comb)
+        for comb in combinations(data, comb_len)
+        if sum(comb) == target_sum
+    )
+
+
+def main():
     with open("inputs/day1.txt") as f:
         data = [int(x) for x in f]
     print(solve_day1(data, 2, 2020))
---
+++ day2.py
@@ -0,0 +1,39 @@
+def process_line(ln):
+    policy, letter, pwd = ln.split()
+    policy = get_policy_range(policy)
+    letter = letter[0]
+    return policy, letter, pwd
+
+
+def check_pwd_policy1(processed):
+    policy, letter, pwd = processed
+    return pwd.count(letter) in policy
+
+
+def check_pwd_policy2(processed):
+    policy, letter, pwd = processed
+    idx1 = policy[0] - 1
+    idx2 = policy[-1] - 1
+    return (pwd[idx1] == letter) ^ (pwd[idx2] == letter)
+
+
+def get_policy_range(data):
+    vals = [int(val) for val in data.split("-")]
+    return range(vals[0], vals[1] + 1)
+
+
+def solve_day2(data):
+    part1 = sum(check_pwd_policy1(ln) for ln in data)
+    part2 = sum(check_pwd_policy2(ln) for ln in data)
+    return part1, part2
+
+
+def main():
+    with open("inputs/day2.txt") as f:
+        lines = [process_line(line) for line in f]
+
+    print(solve_day2(lines))
+
+
+if __name__ == "__main__":
+    main()
