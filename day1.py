ac3a9a98f60b5b679e87e1e918c64f0e241958de
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2020-12-02 22:38:49 -0500

    Better solution to day1 puzzle

--- day1.py
+++ day1.py
@@ -1,35 +1,28 @@
-def solve_day1_1(data):
-    """
-    Solves day1-1 puzzle
-
-    >>> solve_day1_1([1721, 979, 366, 299, 675, 1456])
-    514579
-    """
-    for i, val1 in enumerate(data):
-        for val2 in data[i+1:]:
-            if val1 + val2 == 2020:
-                return val1 * val2
+from itertools import combinations
+from math import prod
 
 
-def solve_day1_2(data):
-    """
-    Solves day1-2 puzzle
-
-    >>> solve_day1_2([1721, 979, 366, 299, 675, 1456])
-    241861950
-    """
-    for i, val1 in enumerate(data):
-        for j, val2 in enumerate(data[i+1:]):
-            for val3 in data[j+1:]:
-                if val1 + val2 + val3 == 2020:
-                    return val1 * val2 * val3
+def solve_day1(data, comb_len, target_sum):
+    return next(
+        prod(comb)
+        for comb in combinations(data, comb_len)
+        if sum(comb) == target_sum
+    )
 
 
 def main():
+    """
+    Solves day1 puzzle
+    >>> in_data = [1721, 979, 366, 299, 675, 1456]
+    >>> solve_day1(in_data, 2, 2020)
+    514579
+    >>> solve_day1(in_data, 3, 2020)
+    241861950
+    """
     with open("inputs/day1.txt") as f:
         data = [int(x) for x in f]
-    print(solve_day1_1(data))
-    print(solve_day1_2(data))
+    print(solve_day1(data, 2, 2020))
+    print(solve_day1(data, 3, 2020))
 
 
 if __name__ == "__main__":
