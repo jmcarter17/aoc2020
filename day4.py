2a8c007b9530e68d1cbdb3f889fc8fc87467e900
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2020-12-04 09:35:09 -0500

    Refactoring of day4 --- cleaner code

--- day4.py
+++ day4.py
@@ -5,100 +5,70 @@ def process_line(ln):
     return ln
 
 
+def check_byr(byr):
+    return 1920 <= int(byr) <= 2002
+
+
+def check_iyr(iyr):
+    return 2010 <= int(iyr) <= 2020
+
+
+def check_eyr(eyr):
+    return 2020 <= int(eyr) <= 2030
+
+
+def check_hgt(hgt):
+    return any(
+        (
+            hgt[2:] == "in" and 59 <= int(hgt[:2]) <= 76,
+            hgt[3:] == "cm" and 150 <= int(hgt[:3]) <= 193,
+        )
+    )
+
+
+def check_hcl(hcl):
+    return all(
+        (hcl[0] == "#", len(hcl) == 7, all(c in "0123456789abcdef" for c in hcl[1:]))
+    )
+
+
+def check_ecl(ecl):
+    return ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+
+def check_pid(pid):
+    return len(pid) == 9 and int(pid) > -1
+
+
 @timer
 def solve_part1(data):
-    required = ["byr", 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    valids = []
-    for pp in data:
-        valid = True
-        for req in required:
-            if req not in pp:
-                valid = False
-                break
-        if valid:
-            valids.append(pp)
+    required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    valids = [pp for pp in data if all(req in pp for req in required)]
 
     return valids
-
-
-def check_byr(pp):
-    try:
-        return 1920 <= int(pp['byr']) <= 2002
-    except Exception:
-        return False
-
-
-def check_iyr(pp):
-    try:
-        return 2010 <= int(pp['iyr']) <= 2020
-    except Exception:
-        return False
-
-
-def check_eyr(pp):
-    try:
-        return 2020 <= int(pp['eyr']) <= 2030
-    except Exception:
-        return False
-
-
-def check_hgt(pp):
-    try:
-        if len(pp['hgt']) == 4:
-            return pp['hgt'][2:] == 'in' and 59 <= int(pp['hgt'][:2]) <= 76
-        elif len(pp['hgt']) == 5:
-            return pp['hgt'][3:] == 'cm' and 150 <= int(pp['hgt'][:3]) <= 193
-        else:
-            return False
-    except Exception:
-        return False
-
-
-def check_hcl(pp):
-    try:
-        if len(pp['hcl']) != 7 or pp['hcl'][0] != '#':
-            return False
-        else:
-            for c in pp['hcl'][1:]:
-                if c not in '0123456789abcdef':
-                    return False
-            return True
-    except Exception:
-        return False
-
-
-def check_ecl(pp):
-    try:
-        if pp['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-            return False
-        return True
-    except Exception:
-        return False
-
-
-def check_pid(pp):
-    try:
-        if len(pp['pid']) != 9:
-            return False
-        id = int(pp['pid'])
-        return id != -1
-    except Exception:
-        return False
-
 
 
 @timer
-def solve_part2(data, validpp):
-    valids = 0
-    for pp in validpp:
-        if all((check_byr(pp), check_ecl(pp), check_eyr(pp), check_hcl(pp), check_hgt(pp), check_iyr(pp), check_pid(pp))):
-            valids += 1
-    return valids
+def solve_part2(data):
+    return sum(
+        all(
+            (
+                check_byr(pp["byr"]),
+                check_ecl(pp["ecl"]),
+                check_eyr(pp["eyr"]),
+                check_hcl(pp["hcl"]),
+                check_hgt(pp["hgt"]),
+                check_iyr(pp["iyr"]),
+                check_pid(pp["pid"]),
+            )
+        )
+        for pp in data
+    )
 
 
 def solve_day4(data):
     part1 = solve_part1(data)
-    part2 = solve_part2(data, part1)
+    part2 = solve_part2(part1)
 
     return len(part1), part2
 
