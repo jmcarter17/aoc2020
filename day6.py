a0a637f8dda91499cfc4ddda579321332d51b5bb
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2020-12-06 00:58:40 -0500

    Refactor day6 -- cleaner

--- day6.py
+++ day6.py
@@ -4,21 +4,17 @@ from utils import timer
 def solve_day6(data):
     part1 = []
     part2 = []
-    set1 = set()
-    set2 = set(list('abcdefghijklmnopqrstuvwxyz'))
+    group = []
     for ln in data:
-        if ln == '':
-            part1.append(set1)
-            part2.append(set2)
-            set1 = set()
-            set2 = set(list('abcdefghijklmnopqrstuvwxyz'))
+        if ln == set():
+            part1.append(set.union(*group))
+            part2.append(set.intersection(*group))
+            group.clear()
         else:
-            setln = set(list(ln))
-            set1 = set1.union(setln)
-            set2 = set2.intersection(setln)
+            group.append(ln)
 
-    part1.append(set1)
-    part2.append(set2)
+    part1.append(set.union(*group))
+    part2.append(set.intersection(*group))
 
     result1 = sum(len(group) for group in part1)
     result2 = sum(len(group) for group in part2)
@@ -29,7 +25,7 @@ def solve_day6(data):
 @timer
 def main():
     with open("inputs/day6.txt") as f:
-        data = [ln.strip() for ln in f]
+        data = [set(ln.strip()) for ln in f]
 
     print(solve_day6(data))
 
