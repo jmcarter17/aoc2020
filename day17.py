26e03497f2c51e54c822311fe5265644f4eb8cca
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2020-12-18 01:08:48 -0500

    General Solution for conway in multi-dimensions

--- day17.py
+++ day17.py
@@ -1,4 +1,5 @@
-from itertools import count
+from itertools import product
+
 import numpy as np
 
 from utils import timer
@@ -48,7 +49,7 @@ def part2(data):
 def hyper_conway(data):
     data = pad_data(data)
     copy = np.copy(data)
-    it = np.nditer(data, flags=['multi_index'])
+    it = np.nditer(data, flags=["multi_index"])
     while not it.finished:
         idx = it.multi_index
         num_on = count_on_neighbors(copy, idx)
@@ -72,42 +73,10 @@ def count_on_neighbors(data, idx):
 
 
 def get_neighbors(data, idx):
-    if len(data.shape) == 3:
-        return get_neighbors_3d(data, idx)
-    elif len(data.shape) == 4:
-        return get_neighbors_4d(data, idx)
-
-
-def get_neighbors_3d(data, idx):
-    i, j, k = idx
-    xrange = range(max(0, i - 1), min(data.shape[0], i + 2))
-    yrange = range(max(0, j - 1), min(data.shape[1], j + 2))
-    zrange = range(max(0, k - 1), min(data.shape[2], k + 2))
-
-    return (
-        (x, y, z)
-        for x in xrange
-        for y in yrange
-        for z in zrange
-        if (x, y, z) != (i, j, k)
+    ranges = (
+        range(max(0, d - 1), min(data.shape[i], d + 2)) for i, d in enumerate(list(idx))
     )
-
-
-def get_neighbors_4d(data, idx):
-    i, j, k, l = idx
-    xrange = range(max(0, i - 1), min(data.shape[0], i + 2))
-    yrange = range(max(0, j - 1), min(data.shape[1], j + 2))
-    zrange = range(max(0, k - 1), min(data.shape[2], k + 2))
-    wrange = range(max(0, l - 1), min(data.shape[3], l + 2))
-
-    return (
-        (x, y, z, w)
-        for x in xrange
-        for y in yrange
-        for z in zrange
-        for w in wrange
-        if (x, y, z, w) != (i, j, k, l)
-    )
+    return (x for x in product(*ranges) if x != idx)
 
 
 if __name__ == "__main__":
